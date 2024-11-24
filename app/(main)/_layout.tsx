d6f1949a238a669c314bd05b31647d74c9885d5b
etugbeh-plateaumed <etugbeh@plateaumed.com>
2024-11-24 21:45:01 +0100

    chore: add icon assets

--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -1,12 +1,34 @@
 import { Tabs } from "expo-router";
+//
+import {
+  HandshakeIcon,
+  HandshakeOutlineIcon,
+  HomeIcon,
+  HomeOutlineIcon,
+} from "@/constants/ICON";
 
 export default function MainLayout() {
   console.log("🚀 ~ MainLayout");
   // renders
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ headerShown: false }} />
-      <Tabs.Screen name="bills" options={{ title: "Bills" }} />
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerShown: false,
+          title: "Home",
+          tabBarIcon: ({ color, focused }) =>
+            focused ? <HomeIcon /> : <HomeOutlineIcon />,
+        }}
+      />
+      <Tabs.Screen
+        name="bills/index"
+        options={{
+          title: "Bills",
+          tabBarIcon: ({ color, focused }) =>
+            focused ? <HandshakeIcon /> : <HandshakeOutlineIcon />,
+        }}
+      />
     </Tabs>
   );
 }
---
+++ constants/ICON.ts
@@ -0,0 +1,4 @@
+export { default as HandshakeOutlineIcon } from "../assets/icons/handshake-outline.svg";
+export { default as HandshakeIcon } from "../assets/icons/handshake.svg";
+export { default as HomeOutlineIcon } from "../assets/icons/home-outline.svg";
+export { default as HomeIcon } from "../assets/icons/home.svg";
