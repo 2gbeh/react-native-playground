b9b45fc0fed666761f4451e35308e373774ec54b
etugbeh-plateaumed <etugbeh@plateaumed.com>
2024-12-12 20:20:16 +0100

    chore: add redux, rmv prisma

--- store/services/accounts/account.service.ts
+++ store/services/accounts/account.service.ts
@@ -1,16 +1,6 @@
-import { prisma } from "@/lib/prisma/prisma.config";
 
 export class AccountService {
   static async getAll() {
-    // const newUser = await prismaClient.user.create({
-    //   data: {
-    //     name: "Elliott",
-    //     email: "xelliottx@example-user.com",
-    //   },
-    // });
 
-    const users = await prisma.shopera_products.findMany();
-    // console.log("🚀 ~ AccountService ~ getAll ~ users:", users)
-    return users
   }
 }
